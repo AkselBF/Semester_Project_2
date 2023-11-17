352c8d2a526badfdb4dc5122397a98a1ebc541b8
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-11-17 10:02:50 +0100

    testing correct push

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html"],
   theme: {
     extend: {},
+    colors: {
+      'white': '#ffffff',
+      'black': '#000000',
+      'dark_gray': '#1E1E1E',
+      'darker_gray': '#171717',
+      'login_blue': '#3A7DFFD1',
+      'login_second': '#002978B3'
+    },
   },
   plugins: [],
 }
