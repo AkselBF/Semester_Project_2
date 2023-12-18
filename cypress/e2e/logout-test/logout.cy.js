44c957ac45dc2e06c0e4b60fac5bd94fc5e5cb40
AkselBF <aksel.boutrouefaret@yahoo.com>
2023-12-18 15:56:26 +0100

    remade the e2e tests

--- cypress/e2e/logout-test/logout.cy.js
+++ cypress/e2e/logout-test/logout.cy.js
@@ -1,21 +1,21 @@
 describe('User Logout', () => {
   it('logs the user out when clicking the logout button', () => {
-    cy.visit('http://127.0.0.1:5500/');
+    cy.visit('index.html');
 
     cy.wait(1000);
 
     cy.get('#login_button').click();
 
-    cy.wait(1000);
-
     // Fill out the login form
     cy.get('#login_email').type('ten@stud.noroff.no');
     cy.get('#login_password').type('UserTen10');
-    cy.wait(1000);
+    cy.wait(500);
 
     cy.get('#log_form').submit();
 
-    cy.wait(2000);
+    cy.get('#user_profile').contains('userTen');
+
+    cy.wait(500);
 
     // Clicks on the logout button
     cy.get('#logout').click();
